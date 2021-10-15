e6864917ebfd17013ef3248f8c8c8159a6bf57bc
lagmanzaza <lagmanmc@gmail.com>
2021-10-15 15:36:26 +0700

    feat: i18n

--- Hearths/frontend/pages/_app.tsx
+++ Hearths/frontend/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import NextApp, { AppInitialProps } from "next/app";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import { appWithTranslation } from "next-i18next";
+import { Router } from "next/dist/client/router";
+import { AppContextType } from "next/dist/shared/lib/utils";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,4 +14,11 @@ function MyApp({ Component, pageProps }: AppProps) {
     </ChakraProvider>
   );
 }
-export default MyApp;
+MyApp.getInitialProps = async (appContext: AppContextType<Router>) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps: AppInitialProps = await NextApp.getInitialProps(appContext);
+
+  return { ...appProps };
+};
+
+export default appWithTranslation(MyApp);
--- Hearths/frontend/pages/index.tsx
+++ Hearths/frontend/pages/index.tsx
@@ -1,21 +1,39 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { HeartInput } from "../components/element/Input";
 import TopMenu from "../components/topmenu";
 
 const Home: NextPage = () => {
+  const { t, i18n } = useTranslation();
   return (
     <Box>
       <TopMenu />
       <Box width={["100%", "30%"]} mx="auto">
+        {t("test")}
         <HeartInput
           placeholder="User name"
           toolTipText="It is used for your identification and is used only 
 in this process. It will not be used for anything else"
         />
+        <Button
+          onClick={() => {
+            console.log("i", i18n);
+            i18n.changeLanguage(i18n.language === "en" ? "th" : "en");
+          }}
+        >
+          Change Language
+        </Button>
       </Box>
     </Box>
   );
 };
+
+export const getStaticProps = async ({ locale }: any) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["common"])),
+  },
+});
 
 export default Home;
