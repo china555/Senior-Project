ce63c729bf6fa99688e51f3c24743d82c33c9c06
lagmanzaza <lagmanmc@gmail.com>
2021-10-23 22:55:59 +0700

    feat: save locale to localstorage

--- Hearths/frontend/pages/_app.tsx
+++ Hearths/frontend/pages/_app.tsx
@@ -3,14 +3,31 @@ import type { AppProps } from "next/app";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../utils/";
-import React from "react";
-import { store, StoreonContext } from "../store";
+import React, { useEffect } from "react";
+import { store, StoreonContext, useAppStore } from "../store";
+import { isBrowser } from "../utils/isBrowser";
+import { Language } from "../store/locale/@types";
+
+function RenderComponent({
+  Component,
+  pageProps,
+}: Pick<AppProps, "Component" | "pageProps">) {
+  const { dispatch } = useAppStore();
+  useEffect(() => {
+    if (!isBrowser) return;
+    const language = localStorage.getItem("language") as Language;
+    if (!language) return;
+    dispatch("locale/changeLocale", language);
+  }, []);
+
+  return <Component {...pageProps} />;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StoreonContext.Provider value={store}>
       <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+        <RenderComponent Component={Component} pageProps={pageProps} />
       </ChakraProvider>
     </StoreonContext.Provider>
   );
