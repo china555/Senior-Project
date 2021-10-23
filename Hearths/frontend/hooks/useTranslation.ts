1c4d7532c91a5006f1090c2f15d5f20792110d57
lagmanzaza <lagmanmc@gmail.com>
2021-10-23 21:51:07 +0700

    refactor: return type

--- Hearths/frontend/hooks/useTranslation.ts
+++ Hearths/frontend/hooks/useTranslation.ts
@@ -2,7 +2,7 @@ import { useCallback, useMemo } from "react";
 import { useAppStore } from "../store";
 import { ILocaleState, Language } from "../store/locale/@types";
 
-export const useTranslation = (keys: (keyof ILocaleState)[]) => {
+export const useTranslation = <T extends keyof ILocaleState>(keys: T[]) => {
   const { dispatch, ...locales } = useAppStore("currentLocale", ...keys);
   const changeLocale = useCallback(
     (language: Language) => {
@@ -14,11 +14,8 @@ export const useTranslation = (keys: (keyof ILocaleState)[]) => {
   return useMemo(
     () => ({
       translations: Object.entries(locales)
-        .filter(([key]) => keys.includes(key as keyof ILocaleState))
-        .reduce((a, [k, v]) => ({ ...a, [k]: v }), {}) as Record<
-        keyof ILocaleState,
-        string
-      >,
+        .filter(([key]) => keys.includes(key as T))
+        .reduce((a, [k, v]) => ({ ...a, [k]: v }), {}) as Record<T, string>,
       changeLocale,
     }),
     [changeLocale, keys, locales]
