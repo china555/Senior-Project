6793439f8d9c738eb6bb6e07189528e0fa5f23d8
china <kittikornchina2014@gmail.com>
2021-12-09 23:14:47 +0700

    update

--- Hearths/frontend/pages/our-team/physiotherapist.tsx
+++ Hearths/frontend/pages/our-team/physiotherapist.tsx
@@ -13,58 +13,59 @@ import { HeartsLayouts } from "../../layouts/layout";
 import HeartsContainer from "../../components/common/HeartsContainer";
 import { useState, useEffect } from "react";
 import { useTranslation } from "../../hooks/useTranslation";
+import { IPhysiotherapist } from "../../store/locale/@types";
 
 const PhysioTherapist: NextPage = () => {
   const { translations } = useTranslation("Physiotherapist", "DepartMentName");
-  const [selectedDepartMent, setselectedDepartMent] = useState("All");
+  const [selectedDepartMent, setselectedDepartMent] = useState("10");
   const onchangeHandler = (event: any) => {
     setselectedDepartMent(event.target.value);
   };
-
-  const departmentofPhysiotherapist = {
-    OT: {
+  type Department = {
+    name: string;
+    staff: IPhysiotherapist[];
+  };
+  const departmentofPhysiotherapist: Department[] = [
+    {
       name: translations.DepartMentName.OccupationalTherapy,
       staff: translations.Physiotherapist.PhysiotherapistOT,
     },
-    MS: {
+    {
       name: translations.DepartMentName.MusculoSkeletalSystem,
       staff: translations.Physiotherapist.PhysiotherapistMS,
     },
-    NS: {
+    {
       name: translations.DepartMentName.NeurologicalSystem,
       staff: translations.Physiotherapist.PhysiotherapistNS,
     },
-    P: {
+    {
       name: translations.DepartMentName.Pediatric,
       staff: translations.Physiotherapist.PhysiotherapistPediatric,
     },
-    S: {
+    {
       name: translations.DepartMentName.Scoliosis,
       staff: translations.Physiotherapist.PhysiotherapistScoliosis,
     },
-    WH: {
+    {
       name: translations.DepartMentName.WomenHealth,
       staff: translations.Physiotherapist.PhysiotherapistWomen,
     },
-    C: {
+    {
       name: translations.DepartMentName.Community,
       staff: translations.Physiotherapist.PhysiotherapistCommunity,
     },
-  };
+  ];
 
-  const [department, setDepartMent] = useState<any>(
+  const [department, setDepartMent] = useState<Department[]>(
     Object.values(departmentofPhysiotherapist)
   );
 
   useEffect(() => {
-    if (selectedDepartMent === "All") {
+    if (selectedDepartMent === "10") {
       setDepartMent(Object.values(departmentofPhysiotherapist));
     } else {
-      setDepartMent([departmentofPhysiotherapist[selectedDepartMent]]);
+      setDepartMent([departmentofPhysiotherapist[Number(selectedDepartMent)]]);
     }
-
-    // setDepartMent(temp);
-    // console.log(department);
   }, [selectedDepartMent]);
 
   return (
@@ -75,25 +76,23 @@ const PhysioTherapist: NextPage = () => {
             Our Team
           </Heading>
           <Select onChange={onchangeHandler} value={selectedDepartMent}>
-            <option value="All">All</option>
-            <option value="OT">
+            <option value={10}>All</option>
+            <option value={0}>
               {translations.DepartMentName.OccupationalTherapy}
             </option>
-            <option value="MS">
+            <option value={1}>
               {translations.DepartMentName.MusculoSkeletalSystem}
             </option>
-            <option value="NS">
+            <option value={2}>
               {translations.DepartMentName.NeurologicalSystem}
             </option>
-            <option value="P">{translations.DepartMentName.Pediatric}</option>
-            <option value="S">{translations.DepartMentName.Scoliosis}</option>
-            <option value="WH">
-              {translations.DepartMentName.WomenHealth}
-            </option>
-            <option value="C">{translations.DepartMentName.Community}</option>
+            <option value={3}>{translations.DepartMentName.Pediatric}</option>
+            <option value={4}>{translations.DepartMentName.Scoliosis}</option>
+            <option value={5}>{translations.DepartMentName.WomenHealth}</option>
+            <option value={6}>{translations.DepartMentName.Community}</option>
           </Select>
         </Box>
-        {department.map((d: any) => (
+        {department.map((d: Department) => (
           <Box mt="4" key={d.name}>
             <Box
               fontSize={{ base: "26px", lg: "30px", xl: "30px" }}
@@ -110,7 +109,7 @@ const PhysioTherapist: NextPage = () => {
               justifyContent="space-between"
               flexWrap="wrap"
             >
-              {d.staff.map((staffName: any) => (
+              {d.staff.map((staffName: IPhysiotherapist) => (
                 <Box
                   key={staffName.name}
                   w={{ base: "100%", xl: "45%" }}
