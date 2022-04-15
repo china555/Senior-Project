f39f7d5575c2113aa9571f0fe45b525cc04b74e3
china555 <kittikornchina2014@gmail.com>
2022-04-15 16:17:32 +0700

    update meta tag

--- Hearths/pages/_app.tsx
+++ Hearths/pages/_app.tsx
@@ -5,15 +5,28 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../utils/";
 import React from "react";
 import { store, StoreonContext, useAppStore } from "../store";
-
+import Head from "next/head";
 import dynamic from "next/dynamic";
 
-const RenderComponent = dynamic(import("../components/shared/RenderComponent"));
+const RenderComponent = dynamic(
+  import("../components/shared/RenderComponent"),
+  { ssr: false }
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StoreonContext.Provider value={store}>
       <ChakraProvider theme={theme}>
+        <Head>
+          <title>HeaRTS</title>
+          <meta charSet="UTF-8" />
+          <meta name="keywords" content="titla, meta, nextjs,HeaRTS,Hearts" />
+          <meta property="og:image" content="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <RenderComponent Component={Component} pageProps={pageProps} />
       </ChakraProvider>
     </StoreonContext.Provider>
