e7b88e07b1ba9e421ffeb7be8e0f091649858a35
china555 <kittikornchina2014@gmail.com>
2022-04-15 15:22:14 +0700

    update url https for using backend system

--- Hearths/constant.ts
+++ Hearths/constant.ts
@@ -4,7 +4,7 @@
 
 // export const url = "http://localhost:3030";
 
-export const url = "https://159.223.47.213";
+export const url = "https://tptbackend.chinaapanda.com";
 
 export const webex_token =
   "MDVkMGYyY2MtMmYyMC00OTFhLTliOTEtYjM1MmI1ODhlNGJmNGRlNGYwNTAtYWNk_PF84_a01480d8-6f60-461b-bc2a-6a57cea93eb2";
